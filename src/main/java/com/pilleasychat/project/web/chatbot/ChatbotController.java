e0af979768e8424916664e8fbd505af5cf05b7c9
ggom121@naver.com <ggom121@naver.com>
2024-04-09 16:30:13 +0900

    feat: API 명세서에 맞게 컨트룰러 수정

--- src/main/java/com/pilleasychat/project/web/chatbot/ChatbotController.java
+++ src/main/java/com/pilleasychat/project/web/chatbot/ChatbotController.java
@@ -5,7 +5,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/chat")
 public class ChatbotController {
     @GetMapping("")
     public String chat() {
--- src/main/java/com/pilleasychat/project/web/login/LoginController.java
+++ src/main/java/com/pilleasychat/project/web/login/LoginController.java
@@ -2,13 +2,21 @@ package com.pilleasychat.project.web.login;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/login")
 public class LoginController {
     @GetMapping("")
-    public String login() {
+    public String loginPage() {
         return "html/login/login";
     }
+
+    @PostMapping("")
+    public String login() {
+        return "/";
+    }
+
+
 }
