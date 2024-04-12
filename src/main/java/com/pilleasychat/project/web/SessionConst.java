ee0e810dd454ca6779c119df6c0f77d415e4ef55
ggom121@naver.com <ggom121@naver.com>
2024-04-12 11:07:22 +0900

    feat: Session, Controller 로직 추가

---
+++ src/main/java/com/pilleasychat/project/web/SessionConst.java
@@ -0,0 +1,5 @@
+package com.pilleasychat.project.web;
+
+public interface SessionConst {
+    String LOGIN_MEMBER = "loginMember";
+}
--- src/main/java/com/pilleasychat/project/web/signup/SignupController.java
+++ src/main/java/com/pilleasychat/project/web/signup/SignupController.java
@@ -1,21 +1,29 @@
 package com.pilleasychat.project.web.signup;
 
+import com.pilleasychat.project.domain.entity.User;
+import com.pilleasychat.project.domain.signup.SignupService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequiredArgsConstructor
 @RequestMapping("/signup")
 public class SignupController {
+
+    private final SignupService signupService;
     @GetMapping("")
     public String signupPage() {
         return "html/signup/signup";
     }
 
     @PostMapping("")
-    public String signup() {
-        return "";
+    public String signup(@ModelAttribute("user") User user) {
+        signupService.register(user);
+        return "redirect:/";
     }
 
     @GetMapping("/additional")
