63d254c00c4d4c8f469a16b4a0f79361b41203e2
ggom121@naver.com <ggom121@naver.com>
2024-04-12 09:20:54 +0900

    feat: 룸북, jpa, h2 의존성 주입

---
+++ src/main/java/com/pilleasychat/project/domain/entity/User.java
@@ -0,0 +1,18 @@
+package com.pilleasychat.project.domain.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "user")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+}
