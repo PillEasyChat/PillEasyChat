d2e8e2b358b891e6c6d5830a4dd2f1f785d79cd6
ggom121@naver.com <ggom121@naver.com>
2024-04-12 09:37:20 +0900

    feat: User entity 생성

--- src/main/java/com/pilleasychat/project/domain/entity/User.java
+++ src/main/java/com/pilleasychat/project/domain/entity/User.java
@@ -7,12 +7,27 @@ import lombok.Setter;
 @Entity
 @Getter
 @Setter
-@Table(name = "user")
+@Table(name = "users")
 public class User {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "id")
     private Long id;
 
+    @Column
+    private String name;
+    @Column
+    private String email;
+    @Column
+    private String password;
+    @Column
+    private String nickname;
+    @Column
+    private int age;
+    @Column
+    private String specialNote;
+    @Column
+    private String allergy;
+    @Column
+    private String takingMedication;
 }
