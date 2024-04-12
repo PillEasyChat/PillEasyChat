ed32dac13a6ec20e5713f213509119252870aa1a
ggom121@naver.com <ggom121@naver.com>
2024-04-12 10:14:35 +0900

    feat: UserRepository, UserService, UserServiceImpl 생성

---
+++ src/main/java/com/pilleasychat/project/domain/user/UserServiceImpl.java
@@ -0,0 +1,40 @@
+package com.pilleasychat.project.domain.user;
+
+import com.pilleasychat.project.domain.entity.User;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Service
+@Slf4j
+@RequiredArgsConstructor
+public class UserServiceImpl implements UserService {
+
+    private final UserRepository userRepository;
+    @Override
+    public void save(User user) {
+        userRepository.save(user);
+    }
+
+    @Override
+    public User findById(Long id) {
+        return userRepository.findById(id).get();
+    }
+
+    @Override
+    public User findByEmail(String email) {
+        return userRepository.findByEmail(email).get();
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        userRepository.deleteById(id);
+    }
+
+    @Override
+    public void update(User user) {
+
+    }
+
+
+}
