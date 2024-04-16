4561b80f7d42f21ef8b7daa4317df4c4c037b14f
ggom121@naver.com <ggom121@naver.com>
2024-04-16 13:29:06 +0900

    feat: 카카오 client_id, redirect_url 설정, 로그인 버튼 추가

---
+++ src/main/java/com/pilleasychat/project/web/login/KakaoLoginController.java
@@ -0,0 +1,9 @@
+package com.pilleasychat.project.web.login;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/login")
+public class KakaoLoginController {
+}
--- src/main/java/com/pilleasychat/project/web/login/LoginController.java
+++ src/main/java/com/pilleasychat/project/web/login/LoginController.java
@@ -6,11 +6,13 @@ import com.pilleasychat.project.web.SessionConst;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
+import org.hibernate.query.sqm.tree.SqmNode;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import static org.hibernate.query.sqm.tree.SqmNode.log;
 
 @Controller
 @RequiredArgsConstructor
@@ -18,8 +20,16 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class LoginController {
 
     private final LoginService loginService;
+
+    @Value("${kakao.client_id}")
+    private String client_id;
+
+    @Value("${kakao.redirect_uri}")
+    private String redirect_uri;
     @GetMapping("")
-    public String loginPage(@ModelAttribute LoginForm loginForm) {
+    public String loginPage(LoginForm loginForm, Model model) {
+        String location = "https://kauth.kakao.com/oauth/authorize?response_type=code&client_id="+client_id+"&redirect_uri="+redirect_uri;
+        model.addAttribute("location", location);
         return "html/login/login";
     }
 
@@ -30,11 +40,15 @@ public class LoginController {
         if (user == null) {
             return "redirect:/login";
         }
-
         HttpSession session = request.getSession();
         session.setAttribute(SessionConst.LOGIN_MEMBER, user);
         return "redirect:/";
     }
 
+    @GetMapping("/callback")
+    public String callback(@RequestParam("code") String code) {
+        System.out.println("sdsd");
+        return null;
+    }
 
 }
