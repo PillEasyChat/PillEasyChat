7223fa05f87971b33a0bb2d83afeaa73175d958c
ggom121@naver.com <ggom121@naver.com>
2024-04-19 10:42:38 +0900

    feat: 구글 로그인 요청 성공

--- src/main/java/com/pilleasychat/project/web/login/GoogleLoginController.java
+++ src/main/java/com/pilleasychat/project/web/login/GoogleLoginController.java
@@ -2,10 +2,19 @@ package com.pilleasychat.project.web.login;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("/login")
 @RequiredArgsConstructor
 public class GoogleLoginController {
+
+    @GetMapping("/oauth2/code/google/{provider}")
+    public String callback(@RequestParam String code){
+
+        return "redirect:/";
+    }
 }
