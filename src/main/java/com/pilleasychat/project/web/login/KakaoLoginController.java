2fe6a99b6a860f24e016b7c7a89cfbb51c5f411a
ggom121@naver.com <ggom121@naver.com>
2024-04-19 09:26:51 +0900

    refact: 카카오 로그인 로직 리팩토링

--- src/main/java/com/pilleasychat/project/web/login/KakaoLoginController.java
+++ src/main/java/com/pilleasychat/project/web/login/KakaoLoginController.java
@@ -1,9 +1,52 @@
 package com.pilleasychat.project.web.login;
 
+import com.pilleasychat.project.domain.entity.User;
+import com.pilleasychat.project.domain.login.KakaoLoginService;
+import com.pilleasychat.project.domain.login.LoginService;
+import com.pilleasychat.project.domain.signup.SignupService;
+import com.pilleasychat.project.domain.user.UserService;
+import com.pilleasychat.project.web.SessionConst;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.beans.factory.annotation.Value;
+
+import java.io.IOException;
+import java.util.HashMap;
 
 @Controller
 @RequestMapping("/login")
+@RequiredArgsConstructor
 public class KakaoLoginController {
+
+    private final LoginService loginService;
+    private final SignupService signupService;
+    private final UserService userService;
+    private final KakaoLoginService kakaoLoginService;
+
+    @Value("${kakao.client_id}")
+    private String client_id;
+
+    @GetMapping("/callback")
+    public String callback(@RequestParam("code") String code, HttpServletRequest request) throws IOException {
+        String accessToken = kakaoLoginService.getAccessTokenFromKakao(client_id, code);
+        HashMap<String, Object> userInfo = kakaoLoginService.getUserInfo(accessToken);
+        System.out.println("email : " + userInfo.get("email"));
+        // User 로그인, 또는 회원가입 로직 추가
+        User user = userService.findByEmail(userInfo.get("email").toString());
+        // 회원가입
+        if (user == null)
+            user = signupService.createUser(
+                    userInfo.get("email").toString(), userInfo.get("nickname").toString());
+        //로그인
+        loginService.login(user.getEmail(), user.getPassword());
+        HttpSession session = request.getSession();
+        session.setAttribute(SessionConst.LOGIN_MEMBER, user);
+
+        return "redirect:/";
+    }
 }
--- src/main/java/com/pilleasychat/project/web/login/LoginController.java
+++ src/main/java/com/pilleasychat/project/web/login/LoginController.java
@@ -1,24 +1,16 @@
 package com.pilleasychat.project.web.login;
 
 import com.pilleasychat.project.domain.entity.User;
-import com.pilleasychat.project.domain.login.KakaoLoginService;
 import com.pilleasychat.project.domain.login.LoginService;
-import com.pilleasychat.project.domain.signup.SignupService;
-import com.pilleasychat.project.domain.user.UserService;
 import com.pilleasychat.project.web.SessionConst;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
-import org.hibernate.query.sqm.tree.SqmNode;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
-import java.io.IOException;
-import java.util.HashMap;
-
-import static org.hibernate.query.sqm.tree.SqmNode.log;
 
 @Controller
 @RequiredArgsConstructor
@@ -26,9 +18,6 @@ import static org.hibernate.query.sqm.tree.SqmNode.log;
 public class LoginController {
 
     private final LoginService loginService;
-    private final SignupService signupService;
-    private final UserService userService;
-    private final KakaoLoginService kakaoLoginService;
 
     @Value("${kakao.client_id}")
     private String client_id;
@@ -51,25 +40,6 @@ public class LoginController {
         }
         HttpSession session = request.getSession();
         session.setAttribute(SessionConst.LOGIN_MEMBER, user);
-        return "redirect:/";
-    }
-
-    @GetMapping("/callback")
-    public String callback(@RequestParam("code") String code, HttpServletRequest request) throws IOException {
-        String accessToken = kakaoLoginService.getAccessTokenFromKakao(client_id, code);
-        HashMap<String, Object> userInfo = kakaoLoginService.getUserInfo(accessToken);
-        System.out.println("email : " + userInfo.get("email"));
-        // User 로그인, 또는 회원가입 로직 추가
-        User user = userService.findByEmail(userInfo.get("email").toString());
-        // 회원가입
-        if (user == null)
-            user = signupService.createUser(
-                            userInfo.get("email").toString(), userInfo.get("nickname").toString());
-        //로그인
-        loginService.login(user.getEmail(), user.getPassword());
-        HttpSession session = request.getSession();
-        session.setAttribute(SessionConst.LOGIN_MEMBER, user);
-
         return "redirect:/";
     }
 
