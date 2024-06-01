0e3ac560caa4b7eb1f29e8c68a58edd7c5b9d79a
ggom121@naver.com <ggom121@naver.com>
2024-06-02 01:23:18 +0900

    feat: 마이페이지로 user 넘겨주기 구현

--- src/main/java/com/pilleasychat/project/web/mypage/MypageController.java
+++ src/main/java/com/pilleasychat/project/web/mypage/MypageController.java
@@ -1,20 +1,35 @@
 package com.pilleasychat.project.web.mypage;
 
+import com.pilleasychat.project.domain.entity.User;
+import com.pilleasychat.project.domain.user.UserService;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/mypage")
+@RequiredArgsConstructor
 public class MypageController {
+
+    private final UserService userService;
     @GetMapping("")
-    public String mypage() {
+    public String mypage(Model model,  HttpServletRequest request) {
+        HttpSession session = request.getSession();
+        User user = userService.findByEmail((String)session.getAttribute("userEmail"));
+        model.addAttribute(user);
         return "html/mypage/mypage";
     }
 
     @GetMapping("/update")
-    public String updatePage() {
+    public String updatePage(Model model,HttpServletRequest request) {
+        HttpSession session = request.getSession();
+        User user = userService.findByEmail((String)session.getAttribute("userEmail"));
+        model.addAttribute(user);
         return "html/mypage/update";
     }
 
